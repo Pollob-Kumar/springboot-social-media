552afdc379a41c41537184f63ca2df7913b28905
Pollob kumar <pkspollob14@gmail.com>
2025-08-29 00:34:35 +0600

    added controller and models package.

---
+++ Pollob-social-media/src/test/java/com/pollob/PollobSocialMediaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pollob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//Ata automatically create kore(folder, package(project create er somoy je package nam dichi), and ai class)
+@SpringBootTest
+class RafiSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
